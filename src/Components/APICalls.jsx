2326b4614fb062d1775fb8a9bd853bcfeaac965a
SRIRAM GOKUL <sriramgokul22@gmail.com>
2024-09-02 11:31:39 +0530

    Created a  the API Calls functions

---
+++ src/Components/APICalls.jsx
@@ -0,0 +1,52 @@
+import { API } from "./API";
+
+// To get all the users details from API
+export async function getAllUsers(){
+    const res = await fetch(API,{
+        method: "GET",
+    });
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
+
+// To delete a details from the users
+export async function DeleteUsers(id){
+    const res = await fetch(`${API}/${id}`,{
+        method : "DELETE",
+        headers : {
+            "Content-type": "application/json",
+        },
+        })
+        const data = await res.json();
+        return data;
+    }
+
+
+  // To Add new users in the API  
+ export async function addNewUsers(newObj){
+    const res = await fetch(`${API}`,{
+        method: "POST",
+        body: JSON.stringify(),
+        headers : {
+            "Content-type": "application/json",
+        },
+    })
+    const data = await res.json();
+    return data;
+ }  
+ 
+  // To Edit new users in the API  
+
+ export async function editUsers(id,editDoc){
+    const res = await fetch(`${API}/${id}`,{    
+        method: "PUT",
+        body: JSON.stringify(editDoc),
+        headers : {
+            "Content-type": "application/json",
+        },
+
+    })
+    const data = await res.json();
+    return data;
+ }
